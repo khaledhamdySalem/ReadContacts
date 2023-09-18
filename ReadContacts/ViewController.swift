6d15ea32d748be5f63f7e0161cc9c91d5334f712
Khaled <Khaled.hamdy2290@gmail.com>
2023-09-18 20:00:51 +0300

    Initial Commit

---
+++ ReadContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadContacts
+//
+//  Created by Khaled Hamdy on 18/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
